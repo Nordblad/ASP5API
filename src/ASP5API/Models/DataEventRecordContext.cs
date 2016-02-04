984b13165c4716ef95f4f5abeb7a25bb3f9b6c5a
Olov Nordblad <olle.nordblad@gmail.com>
2016-02-04 19:23:47 +0100

    SQLite added

---
+++ src/ASP5API/Models/DataEventRecordContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP5API.Models
+{
+    using Microsoft.Data.Entity;
+
+    // >dnx . ef migrations add testMigration
+    public class DataEventRecordContext : DbContext
+    {
+        public DbSet<DataEventRecord> DataEventRecords { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<DataEventRecord>().HasKey(m => m.Id);
+            base.OnModelCreating(builder);
+        }
+    }
+}
